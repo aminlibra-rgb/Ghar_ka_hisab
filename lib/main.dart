4194817da1997a3f404bbc3011cf063a2b5628c4
Amin <you@example.com>
2026-07-19 06:28:58 +0500

    Remove DefaultCupertinoLocalizations delegate permanently

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,6 @@ class GharKaHisabApp extends StatelessWidget {
             localizationsDelegates: const [
               DefaultMaterialLocalizations.delegate,
               DefaultWidgetsLocalizations.delegate,
-              DefaultCupertinoLocalizations.delegate,
             ],
             home: const Directionality(
               textDirection: TextDirection.rtl,
